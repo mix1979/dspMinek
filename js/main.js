58149a59e22cbf9d5e4906dcb71fdd70a1246ff9
mix1979 <97940470+mix1979@users.noreply.github.com>
2025-02-26 08:43:05 +0900

    1233

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ elements.passwordField.addEventListener(
 
 function enterJournal() {
   if (auth.login === "mix1979" && auth.password === "Miha8011979") {
-    window.location.href = "dspMinek/pages/mainPage.html";
+    window.location.href = "/dspMinek/pages/mainPage.html";
   } else {
     window.location.href = "/dspMinek/";
     alert("Неправильный логин или пароль");
